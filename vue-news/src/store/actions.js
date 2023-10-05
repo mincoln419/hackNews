d48396b1f56599e83c91eb17a3aa416763595a48
mingeon cho <mincoln419@naver.com>
2023-10-05 14:12:39 +0900

    no async

--- vue-news/src/store/actions.js
+++ vue-news/src/store/actions.js
@@ -1,7 +1,7 @@
 import { fetchNewsList, fetchJobsList, fetchAsksList, fetchUserInfo, fetchItemInfo, fetchList } from '../api/index.js';
 
 export default {
-    async FETCH_USER(context, userId) {
+    FETCH_USER(context, userId) {
         try{
             const { data } = await fetchUserInfo(userId);
             context.commit('SET_USER', data);
@@ -10,7 +10,7 @@ export default {
             console.log(error);
         }
     }, 
-    async FETCH_ITEM(context, itemId){
+    FETCH_ITEM(context, itemId){
         try{
             const { data } = await fetchItemInfo(itemId);
             context.commit('SET_ITEM', data);
@@ -19,7 +19,7 @@ export default {
             console.log(error);
         }        
     },
-    async FETCH_LIST(context, pageName){
+    FETCH_LIST(context, pageName){
         try{
             const { data } = await fetchList(pageName);
             context.commit('SET_LIST', data);
