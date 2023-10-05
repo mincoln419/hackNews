4e5b2ece55bb8952bd643e60335f05493fcb112d
mingeon cho <mincoln419@naver.com>
2023-10-05 14:21:38 +0900

    bus

--- vue-news/src/components/CreatedListView.js
+++ vue-news/src/components/CreatedListView.js
@@ -1,5 +1,5 @@
 import ListView from './ListView.vue';
-
+import bus from '../utils/bus.js';
 
 export default function createListView(name) {
     return {
@@ -18,7 +18,6 @@ export default function createListView(name) {
               });
             },100);
           },
-
         render(createElement) {
             return createElement(ListView);
         }
