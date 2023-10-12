af2501b313bce9ba075d4b7589b13750799e7248
mingeon cho <mincoln419@naver.com>
2023-10-12 11:03:45 +0900

    event delegation

---
+++ interactive-web/event/ex.js
@@ -0,0 +1,6 @@
+        // {
+        //     const ilbuni = document.querySelector('.ilbuni.c');
+        //     ilbuni.addEventListener('click', () => ilbuni.classList.toggle('special'));
+        // }
+
+        document.querySelector('.characters').innerHTML = '안녕하세요';
